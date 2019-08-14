5ac50019eb3f00a12a8f00afa87979aa20ffd47f
b3nwong <53283480+b3nwong@users.noreply.github.com>
2019-08-14 09:18:12 +0800

    hello

---
+++ prac_01/broken_score.py
@@ -0,0 +1,15 @@
+"""
+CP1404/CP5632 - Practical
+Broken program to determine score status
+"""
+
+score = float(input("Enter score: "))
+if 0<= score <= 100:
+    if score >= 90:
+        print("Excellent")
+    elif score >= 50:
+        print("Passable")
+    elif score <50:
+        print("Bad")
+else:
+    print("Invalid score")
