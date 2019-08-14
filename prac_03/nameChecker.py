11b071d8e715812267c5cde2f79606d6270ddc4a
b3nwong <53283480+b3nwong@users.noreply.github.com>
2019-08-14 10:11:48 +0800

    add name splitter

--- prac_03/nameChecker.py
+++ prac_03/nameChecker.py
@@ -1,9 +1,19 @@
 def main():
-    name= input("Please enter a name")
+    name = get_name()
     while not isValidName(name):
         print("INVALID NAME")
-        name = input("Please enter a name")
-    print(name[::2])
+        name = get_name()
+    print_name_frequency(name,3)
+
+
+def print_name_frequency(name,n):
+    print(name[::n])
+
+
+def get_name():
+    name = input("Please enter a name: ")
+    return name
+
 
 def isValidName(name):
     if len(name)< 1 :
