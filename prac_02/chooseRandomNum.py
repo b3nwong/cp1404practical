1b958cab8410a3dcac6e58cd24fe6da8fa7afc21
b3nwong <53283480+b3nwong@users.noreply.github.com>
2019-08-14 09:18:43 +0800

    hello

---
+++ prac_02/chooseRandomNum.py
@@ -0,0 +1,4 @@
+import random
+print(random.randint(5,20))            #smallest is 5 largest 20
+print(random.randrange(3,10,2))             #smallest is 3 largest is 9; nope 4 is out of step
+print(random.uniform(2.5,5.5))              #smallest 2.5 largest 5.5
