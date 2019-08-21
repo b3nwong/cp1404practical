3f0db7ec2ee6cd2b54fe97af4f291b18421e7875
b3nwong <53283480+b3nwong@users.noreply.github.com>
2019-08-21 11:17:46 +0800

    prac 4

---
+++ prac_04/username_checker.py
@@ -0,0 +1,10 @@
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45',
+'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole',
+'InterpreterInterface', 'StartServer', 'bob']
+
+user_username = input("What is your username?: ")
+
+if user_username in usernames:
+    print("Access granted.")
+else:
+    print("Access denied.")
